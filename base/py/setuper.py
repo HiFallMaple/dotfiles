96cae4fa03a33eb53e456fb0b692987e8dbdabaa
FallMaple <tony53517230@gmail.com>
2024-01-04 18:23:36 +0000

    fix: 修復 git setup

--- base/py/setuper.py
+++ base/py/setuper.py
@@ -1,13 +1,12 @@
-import shutil
 from .config import register, logger
-from pydotfiles import run_command, ROOT, USER_NAME
+from pydotfiles import run_command, ROOT
 
 operate = 'setup'
 
 
-@register.registe_method('ubuntu', operate, "vim")
+@register.registe_method('ubuntu', operate, "git")
 def ubuntu_setup_package():
-    logger.info(f"Setting up vim...")
+    logger.info(f"Setting up git...")
     command = ["git", "config", "--global", "init.defaultBranch", "main"]
     run_command(command, ROOT)
     command = ["git", "config", "--global", "core.editor", "\"vim\""]
