c0366d6d7ec2ea4ff500da1e8cc84715124cfdde
FallMaple <tony53517230@gmail.com>
2024-03-12 22:44:22 +0800

    fix: 修復 bat 在 Ubuntu 下的 link
    bat 在 Ubuntu 23 之後疑似不再需要 link batcat 到 bat
    改為在安裝後立刻判斷並 link

--- bat/py/installer.py
+++ bat/py/installer.py
@@ -1,6 +1,6 @@
 from .config import register, logger
 from pydotfiles import run_command, ROOT
-
+import shutil
 
 operate = 'install'
 package = "bat"
@@ -13,3 +13,9 @@ def ubuntu_install_package():
     logger.info(f"Installing {package}...")
     command = ["apt-get", "install", "-y", package]
     run_command(command, ROOT)
+    
+    if shutil.which(link_dst_name) is None:
+        command = ["ln", "-s", shutil.which(link_src_name), '/bin/bat']
+        run_command(command, ROOT)
+        command = ["ln", "-s", shutil.which(link_src_name), '/usr/bin/bat']
+        run_command(command, ROOT)
