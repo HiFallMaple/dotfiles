a00d524a5459103d20f63fa51d7d8fc13cee00cd
FallMaple <tony53517230@gmail.com>
2024-03-12 22:09:36 +0800

    fix: 修復 python 有 EXTERNALLY-MANAGED 造成的錯誤
    PEP 668 提出全局 pip 和 apt 等包管理器有可能造成衝突
    在 Debian 12 和 Ubuntu 23 開始皆會在 /usr/lib/pythonX.X 目錄下放置 EXTERNALLY-MANAGED
    此 commit 改為若該檔案存在，則將檔案改名為
    EXTERNALLY-MANAGED.bak，在安裝後改回來

--- pyenv/py/installer.py
+++ pyenv/py/installer.py
@@ -1,5 +1,7 @@
 from .config import register, logger, requirements_path
 from pydotfiles import run_command, ROOT, USER
+import sysconfig
+import os
 
 operate = 'install'
 package = 'pyenv'
@@ -8,7 +10,14 @@ package = 'pyenv'
 @register.registe_method('ubuntu', operate, package)
 def ubuntu_install_package():
     logger.info(f"Installing {package}...")
-    command = ["apt-get", "install", "-y", "python3-pip"]
-    run_command(command, ROOT)
+    externally_managed_path = os.path.join(sysconfig.get_path("stdlib", sysconfig.get_default_scheme()), "EXTERNALLY-MANAGED")
+    if os.path.exists(externally_managed_path):
+        logger.info("Python is managed externally. Move EXTERNALLY-MANAGED to EXTERNALLY-MANAGED.bak.")
+        os.rename(externally_managed_path, externally_managed_path + ".bak")
+        
     command = ["python3", "-m", "pip", "install", "-r", requirements_path]
     run_command(command, ROOT)
+    
+    if os.path.exists(externally_managed_path):
+        logger.info("Moving EXTERNALLY-MANAGED.bak back to EXTERNALLY-MANAGED.")
+        os.rename(externally_managed_path + ".bak", externally_managed_path)
