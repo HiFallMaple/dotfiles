ed2872de2cfbf7ed7820b55291e405cd3607e959
FallMaple <tony53517230@gmail.com>
2024-05-13 21:20:06 +0800

    fix: remove link command of bat

--- bat/py/installer.py
+++ bat/py/installer.py
@@ -17,5 +17,3 @@ def ubuntu_install_package():
     if shutil.which(link_dst_name) is None:
         command = ["ln", "-s", shutil.which(link_src_name), '/bin/bat']
         run_command(command, ROOT)
-        command = ["ln", "-s", shutil.which(link_src_name), '/usr/bin/bat']
-        run_command(command, ROOT)
